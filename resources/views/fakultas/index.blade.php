f1ee46c144a31c0ed94ca8c057784fd26b13b8ad
anhar0401 <mfadhilanhar_2428240158@mhs.mdp.ac.id>
2026-05-11 12:10:15 +0700

    main

--- resources/views/fakultas/index.blade.php
+++ resources/views/fakultas/index.blade.php
@@ -1,4 +1,9 @@
+@extends('main.app')
+@section('content')
+    
+@endsection
 <h2>Data Fakultas</h2>
 @foreach ($result as $item)
     {{ $item->nama }} - {{ $item->singkatan }} - {{ $item->dekan }} <br/>
 @endforeach
+@endsection
