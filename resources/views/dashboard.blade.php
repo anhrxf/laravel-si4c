9032943ea6d551343b7c39e007795c2a8923d882
anhar <mfadhilanhar_2428240158@mhs.mdp.ac.id>
2026-06-09 13:06:48 +0700

    dashboard, profile

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,124 +1,17 @@
-@extends('main')
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
 
-@section('content')
-    {{-- ambil dari highchart.js --}}
-
-    {{-- html --}}
-    <script src="https://code.highcharts.com/highcharts.js"></script>
-    <script src="https://code.highcharts.com/modules/exporting.js"></script>
-    <script src="https://code.highcharts.com/modules/export-data.js"></script>
-    <script src="https://code.highcharts.com/modules/accessibility.js"></script>
-    {{-- <figure class="highcharts-figure"> --}}
-        <div class="row">
-            <div class="col-12 col-md-6">
-                <div id="container1"></div> 
-            </div>
-            <div class="col-12 col-md-6">
-                <div id="container2"></div>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __("You're logged in!") }}
+                </div>
             </div>
         </div>
-    {{-- </figure> --}}
-
-    
-    {{-- js --}}
-    <script>
-        // column chart => jumlah mahasiswa per prodi
-        Highcharts.chart('container1', {
-            chart: {
-                type: 'column'
-            },
-            title: {
-                text: 'Grafik Jumlah Mahasiswa UMDP per Program Studi'
-            },
-            subtitle: {
-                text:
-                    'Source: Aplikasi SIMPONI'
-            },
-            xAxis: {
-                categories: [
-                    @foreach ($grafikmhs as $data)
-                        '{{ $data->nama_prodi }}',
-                    @endforeach
-                ],
-                crosshair: true,
-                accessibility: {
-                    description: 'Program Studi'
-                }
-            },
-            yAxis: {
-                min: 0,
-                title: {
-                    text: 'Mahasiswa'
-                }
-            },
-            tooltip: {
-                valueSuffix: ' (orang)'
-            },
-            plotOptions: {
-                column: {
-                    pointPadding: 0.2,
-                    borderWidth: 0
-                }
-            },
-            series: [
-                {
-                    name: 'Mahasiswa',
-                    data: [
-                        @foreach ($grafikmhs as $data)
-                            {{ $data->jumlah_mhs }},
-                        @endforeach
-                    ]
-                }
-            ]
-        });
-         // column chart => jumlah mahasiswa per angkatan
-        Highcharts.chart('container2', {
-            chart: {
-                type: 'column'
-            },
-            title: {
-                text: 'Grafik Jumlah Mahasiswa UMDP per Angakatan'
-            },
-            subtitle: {
-                text:
-                    'Source: Aplikasi SIMPONI'
-            },
-            xAxis: {
-                categories: [
-                    @foreach ($grafik_angkatan as $data)
-                        '{{ $data->angkatan }}',
-                    @endforeach
-                ],
-                crosshair: true,
-                accessibility: {
-                    description: 'Program Studi'
-                }
-            },
-            yAxis: {
-                min: 0,
-                title: {
-                    text: 'Mahasiswa'
-                }
-            },
-            tooltip: {
-                valueSuffix: ' (orang)'
-            },
-            plotOptions: {
-                column: {
-                    pointPadding: 0.2,
-                    borderWidth: 0
-                }
-            },
-            series: [
-                {
-                    name: 'Mahasiswa',
-                    data: [
-                        @foreach ($grafik_angkatan as $data)
-                            {{ $data->jumlah_mhs }},
-                        @endforeach
-                    ]
-                }
-            ]
-        });
-    </script>
-@endsection
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,28 @@
 <?php
-
-use App\Http\Controllers\FakultasController;
-use App\Http\Controllers\PeriodeController;
-use App\Http\Controllers\BeritaController;
-use App\Http\Controllers\ProdiController;
-use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\FakultasController;
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\PeriodeController;
+use App\Http\Controllers\ProdiController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('Fakultas.create');
+    // return view('welcome');
+    return redirect()->route('login');
 });
 
-Route::resource('/fakultas', FakultasController::class)->parameters([
-    'fakultas' => 'fakultas'
-]);
-Route::resource('/prodi', ProdiController::class);
-Route::resource('/periode', PeriodeController::class);
-Route::resource('/berita', BeritaController::class);
-Route::resource('/mahasiswa', MahasiswaController::class);
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->nama('dashboard');
+Route::resource('/fakultas', FakultasController::class)->parameters(['fakultas' => 'fakultas'])->middleware(['auth', 'verified']);
+Route::resource('/periode', PeriodeController::class)->middleware(['auth', 'verified']);
+Route::resource('/prodi', ProdiController::class)->middleware(['auth', 'verified']);
+Route::resource('/mahasiswa', MahasiswaController::class)->middleware(['auth', 'verified']);
 
-route::middleware('auth')->group()
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
